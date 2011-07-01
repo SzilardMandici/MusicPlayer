f37514137f2bd2e6533458eb46d072f97c2f4f53
SzilardMandici <szili_hun@yahoo.com>
2011-07-01 23:31:24 +0300

    new way

--- gen/com/szilard/R.java
+++ gen/com/szilard/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
+<<<<<<< HEAD
         public static final int backgr=0x7f020000;
         public static final int icon=0x7f020001;
         public static final int music_backgr=0x7f020002;
@@ -21,6 +22,9 @@ public final class R {
         public static final int prev=0x7f050000;
         public static final int seekBar=0x7f050004;
         public static final int stop=0x7f050002;
+=======
+        public static final int icon=0x7f020000;
+>>>>>>> 103dac482af960895f58523854529fff3f03075c
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/szilard/MusicPlayerActivity.java
+++ src/com/szilard/MusicPlayerActivity.java
@@ -1,5 +1,6 @@
 package com.szilard;
 
+<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 
@@ -92,3 +93,16 @@ public class MusicPlayerActivity extends Activity implements OnClickListener {
 	
 
 }
+=======
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicPlayerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+>>>>>>> 103dac482af960895f58523854529fff3f03075c
