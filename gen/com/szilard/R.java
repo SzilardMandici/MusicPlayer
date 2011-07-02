8858c44ecdef7167fc52db985f596a815b7e4bc0
SzilardMandici <szili_hun@yahoo.com>
2011-07-02 11:48:17 +0300

    fixing problems

--- gen/com/szilard/R.java
+++ gen/com/szilard/R.java
@@ -21,10 +21,6 @@ public final class R {
         public static final int prev=0x7f050000;
         public static final int seekBar=0x7f050004;
         public static final int stop=0x7f050002;
-        public static final int icon=0x7f020000;
-
-        public static final int icon=0x7f020000;
-
     }
     public static final class layout {
         public static final int main=0x7f030000;
